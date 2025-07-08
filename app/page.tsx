080434aa1b8f7964fc75137ae8b33c6b433440d0
curbfinance <curbfinance.fi@gmail.com>
2025-07-08 15:44:31 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,16 @@
 import dynamic from 'next/dynamic'
+import SwapCard from '@/components/SwapCard'
+import LiquidityCard from '@/components/LiquidityCard'
 
 const App = dynamic(() => import('./app'), { ssr: false })
 export default function Home() {
   return (
+        <div className="max-w-4xl mx-auto p-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+        <SwapCard />
+        <LiquidityCard />
+      </div>
+    </div>
     <>
       <App />
     </>
